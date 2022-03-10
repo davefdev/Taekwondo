9a3556914478e81a95cb997ba95af6f294aaeca6
Davef <76170782+davefdev@users.noreply.github.com>
2022-03-10 22:49:22 +0000

    rtghbrgb

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -33,7 +33,16 @@ const Footer = () => {
               style={{ color: "#ffffff", marginRight: "2.25rem" }}/>taektrainer@pretend.com</h4>
           </div>
         </div>
-        <divclassName
+        <div className="right">
+            <h4>About</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+            <div className="social">
+                <FaFacebook />
+                <FaTwitter />
+                <FaLinkedin/>
+                </div>
+                </div>
       </div>
     </div>
   );
